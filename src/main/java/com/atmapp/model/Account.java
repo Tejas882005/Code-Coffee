e88c6d7dd6d0f538f528feb62de90069f0f25d20
Tejas Nirvan <tejas.24sece1290071@galgotiasuniversity.ac.in>
2025-06-08 01:36:31 +0530

    Change the Account model for the app

--- src/main/java/com/atmapp/model/Account.java
+++ src/main/java/com/atmapp/model/Account.java
@@ -3,44 +3,37 @@ package com.atmapp.model;
 import java.math.BigDecimal;
 
 public class Account {
-    private int accountNumber;
+
+    private int accountId;
     private int userId;
     private BigDecimal checkingBalance;
     private BigDecimal savingsBalance;
 
-    public Account(int accountNumber, int userId, BigDecimal checkingBalance, BigDecimal savingsBalance) {
-        this.accountNumber = accountNumber;
+    public Account(int accountId, int userId, BigDecimal checkingBalance, BigDecimal savingsBalance) {
+        this.accountId = accountId;
         this.userId = userId;
         this.checkingBalance = checkingBalance;
         this.savingsBalance = savingsBalance;
     }
 
-    public int getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(int accountNumber) {
-        this.accountNumber = accountNumber;
+    public int getAccountId() {
+        return accountId;
     }
 
     public int getUserId() {
         return userId;
     }
 
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
     public BigDecimal getCheckingBalance() {
         return checkingBalance;
     }
 
-    public void setCheckingBalance(BigDecimal checkingBalance) {
-        this.checkingBalance = checkingBalance;
-    }
-
     public BigDecimal getSavingsBalance() {
         return savingsBalance;
+    }
+
+    public void setCheckingBalance(BigDecimal checkingBalance) {
+        this.checkingBalance = checkingBalance;
     }
 
     public void setSavingsBalance(BigDecimal savingsBalance) {
