ea344358a9e9cda4d360960355c68edb6a20eb02
Tejas Nirvan <tejas.24sece1290071@galgotiasuniversity.ac.in>
2025-06-08 01:32:48 +0530

    Modify the config Loader to adjust for the changes

--- src/main/java/com/atmapp/util/ConfigLoader.java
+++ src/main/java/com/atmapp/util/ConfigLoader.java
@@ -1,36 +1,20 @@
 package com.atmapp.util;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
-
 public class ConfigLoader {
-    private static ConfigLoader instance;
-    private Properties properties;
+    private static final Properties properties = new Properties();
 
-    private ConfigLoader() {
-        properties = new Properties();
-        try (InputStream input = getClass().getClassLoader().getResourceAsStream("application.properties")) {
-            if (input == null) {
-                throw new IOException("application.properties not found");
-            }
-            properties.load(input);
+    static {
+        try {
+            properties.load(ConfigLoader.class.getClassLoader().getResourceAsStream("application.properties"));
         } catch (IOException e) {
-            throw new RuntimeException("Error loading application.properties", e);
+            throw new RuntimeException("Failed to load configuration", e);
         }
     }
 
-  
-    public static synchronized ConfigLoader getInstance() {
-        if (instance == null) {
-            instance = new ConfigLoader();
-        }
-        return instance;
-    }
-
-    
-    public String getProperty(String key) {
+    public static String getProperty(String key) {
         return properties.getProperty(key);
     }
 }
