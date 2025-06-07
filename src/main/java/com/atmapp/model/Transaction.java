9e24a35cfe5eab67b8afbfa0c8ec9ffc349c2fa7
Tejas Nirvan <tejas.24sece1290071@galgotiasuniversity.ac.in>
2025-06-08 01:29:30 +0530

    Add the constituent getter and setters to the Transaction class

--- src/main/java/com/atmapp/model/Transaction.java
+++ src/main/java/com/atmapp/model/Transaction.java
@@ -11,11 +11,31 @@ public class Transaction {
     private BigDecimal amount;
     private LocalDateTime timestamp;
 
-     public Transaction(int transactionId, int accountId, String type, BigDecimal amount, LocalDateTime timestamp) {
+    public Transaction(int transactionId, int accountId, String type, BigDecimal amount, LocalDateTime timestamp) {
         this.transactionId = transactionId;
         this.accountId = accountId;
         this.type = type;
         this.amount = amount;
         this.timestamp = timestamp;
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
     }
 }
