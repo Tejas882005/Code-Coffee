fff16b4091d3035081ddf25b56f90212518293b0
Tejas Nirvan <tejas.24sece1290071@galgotiasuniversity.ac.in>
2025-06-08 01:34:34 +0530

    Use the db instance in the Userclass

--- src/main/java/com/atmapp/dao/UserDAO.java
+++ src/main/java/com/atmapp/dao/UserDAO.java
@@ -1,29 +1,23 @@
 package com.atmapp.dao;
 
-import com.atmapp.model.User;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.atmapp.model.User;
+
 public class UserDAO {
-    private Connection connection;
-
-    public UserDAO() throws SQLException {
-        this.connection = DBConnection.getInstance();
-    }
-
-    public User findByCustomerNumberAndPin(int customerNumber, int pin) throws SQLException {
-        String query = "SELECT * FROM users WHERE customer_number = ? AND pin = ?";
-        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+    public User findByCustomerNumber(int customerNumber) throws SQLException {
+        String sql = "SELECT * FROM users WHERE customer_number = ?";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setInt(1, customerNumber);
-            stmt.setInt(2, pin);
-            try (ResultSet rs = stmt.executeQuery()) {
-                if (rs.next()) {
-                    return new User(rs.getInt("user_id"), rs.getInt("customer_number"), rs.getInt("pin"));
-                }
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new User(rs.getInt("user_id"), rs.getInt("customer_number"), rs.getInt("pin"));
             }
+            return null;
         }
-        return null; 
     }
 }
